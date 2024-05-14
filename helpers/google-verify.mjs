341e0a834b0b6968a49afbaa2bab9ce8b9e6aca9
elto82 <elto.82@gmail.com>
2024-05-14 15:12:35 -0500

    signin google

--- helpers/google-verify.mjs
+++ helpers/google-verify.mjs
@@ -2,17 +2,13 @@ import { OAuth2Client } from "google-auth-library";
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-export async function googleVerify(token = "") {
+const googleVerify = async (idToken = "") => {
   const ticket = await client.verifyIdToken({
-    idToken: token,
+    idToken,
     audience: process.env.GOOGLE_CLIENT_ID,
   });
-
   const payload = ticket.getPayload();
+  return payload;
+};
 
-  return {
-    name: payload.name,
-    email: payload.email,
-    picture: payload.picture,
-  };
-}
+export { googleVerify };
